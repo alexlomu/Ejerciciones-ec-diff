23f714113c93f395fdc9b9e389f42810535dd3a6
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-10-13 19:06:13 +0200

    ejercicio 2

---
+++ ejercicios.py
@@ -0,0 +1,21 @@
+import latex2sympy2
+import flask
+from sympy import *
+
+init_printing()
+def ej1():
+    x = symbols("x")
+    y = Function("y")
+    CI = {y(3): -1}
+    ed = Eq( y(x).diff(),(y(x)*(x**2)-y(x))/(y(x)+1))
+    print(dsolve(ed, y(x), ics=CI))
+
+
+def ej2():
+    x = symbols("x")
+    y = Function("y")
+    ed = Eq( sin(x)*y(x).diff(),y(x)*ln(y(x)))
+    print(dsolve(ed, y(x)))
+
+
+
